bdd32b0b0e756bcd5a2593f02e03a24beefca3ff
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2026-05-21 20:31:44 +0300

    3043. Find the Length of the Longest Common Prefix (different approach)

--- rust/3043.find-the-length-of-the-longest-common-prefix.rs
+++ rust/3043.find-the-length-of-the-longest-common-prefix.rs
@@ -1,33 +1,23 @@
 use std::collections::HashSet;
+use std::iter::successors;
 
 impl Solution {
     pub fn longest_common_prefix(arr1: Vec<i32>, arr2: Vec<i32>) -> i32 {
-        Self::longest_common_prefix_slice(&arr1, &arr2)
-    }
+        let div10 = |n: &i32| if *n >= 10 { Some(*n / 10) } else { None };
 
-    pub fn longest_common_prefix_slice(arr1: &[i32], arr2: &[i32]) -> i32 {
-        let mut cache = HashSet::new();
+        let prefixes: HashSet<i32> = arr1
+            .iter()
+            .filter(|&&num| num != 0)
+            .flat_map(|&num| successors(Some(num), div10))
+            .collect();
 
-        for &num in arr1 {
-            let mut n = num;
-            while n > 0 {
-                cache.insert(n);
-                n /= 10;
-            }
-        }
-
-        let mut max_len = 0;
-        for &num in arr2 {
-            let mut n = num;
-            while n > 0 {
-                if cache.contains(&n) {
-                    max_len = max_len.max(n.to_string().len() as i32);
-                    break;
-                }
-                n /= 10;
-            }
-        }
-
-        max_len
+        arr2.iter()
+            .filter_map(|&num| {
+                successors(Some(num), div10)
+                    .find(|&prefix| prefixes.contains(&prefix))
+                    .map(|prefix| (prefix.checked_ilog10().unwrap_or_default() as usize) + 1)
+            })
+            .max()
+            .unwrap_or_default() as i32
     }
 }
